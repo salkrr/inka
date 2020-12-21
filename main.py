59a09e635e79bb096d61e4d4d42780bd2e5b8541
lazy-void <salnikov.k54@gmail.com>
2020-12-21 20:59:01 +0300

    Add progress indication

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ from classes.Parser import Parser
 
 def main():
     parser = Parser("test_data/test_1.md")
+
+    print("Getting cards from the file...")
     cards_list = parser.collect_cards()
 
+    # Error
     if cards_list is None:
         return
 
-    if len(cards_list) == 0:
+    number_of_cards = len(cards_list)
+    if number_of_cards == 0:
         print("ERROR: Cards weren't found in file.")
         return
 
+    print(f"Found {number_of_cards} cards!")
+
+    print("Converting cards to the html...")
     Converter.convert_cards(cards_list)
+
+    print("Sending cards...")
     AnkiApi.add_cards(cards_list)
 
 
